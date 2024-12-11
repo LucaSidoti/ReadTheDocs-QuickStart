09a188220a2ea80ae0761ec47fffaeda827e9264
sidoti <luca.sidotipinto@epfl.ch>
2024-12-11 10:40:25 +0100

    PDF

--- source/conf.py
+++ source/conf.py
@@ -1,29 +1,33 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Sphinx to Read the Docs: Complete Guide'
 copyright = '2024, Luca Sidoti'
 author = 'Luca Sidoti'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinx_rtd_theme', 'sphinx_copybutton', 'sphinx_code_tabs', 'sphinx_new_tab_link','sphinx_togglebutton']
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
+    'sphinx_code_tabs',
+    'sphinx_new_tab_link',
+    'sphinx_togglebutton'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Conditional logic to disable problematic extensions for LaTeX builder
+import sys
+from sphinx.application import Sphinx
 
+if isinstance(globals().get('app'), Sphinx) and app.builder.name == "latex":
+    extensions.remove('sphinx_code_tabs')  # Disable unsupported extensions
+    extensions.remove('sphinx_new_tab_link')  # Add more if needed
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
@@ -37,3 +41,11 @@ html_theme_options = {
 
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+
+# -- LaTeX-specific configurations -------------------------------------------
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'figure_align': 'htbp',
+}
